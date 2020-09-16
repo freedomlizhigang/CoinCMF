4e901cab83e3e03a2b66560706f6a2b8f22e4f21
smz_li <lzhigang@shanmuzhi.com>
2020-09-16 10:27:06 +0800

    wechat env

--- config/wechat.php
+++ config/wechat.php
@@ -10,129 +10,129 @@
  */
 
 return [
-    /*
-     * 默认配置，将会合并到各模块中
-     */
-    'defaults' => [
-        /*
-         * Debug 模式，bool 值：true/false
-         *
-         * 当值为 false 时，所有的日志都不会记录
-         */
-        'debug' => true,
+	/*
+		     * 默认配置，将会合并到各模块中
+	*/
+	'defaults' => [
+		/*
+			         * Debug 模式，bool 值：true/false
+			         *
+			         * 当值为 false 时，所有的日志都不会记录
+		*/
+		'debug' => true,
 
-        /*
-         * 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
-         */
-        'response_type' => 'array',
+		/*
+			         * 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+		*/
+		'response_type' => 'array',
 
-        /*
-         * 使用 Laravel 的缓存系统
-         */
-        'use_laravel_cache' => true,
+		/*
+			         * 使用 Laravel 的缓存系统
+		*/
+		'use_laravel_cache' => true,
 
-        /*
-         * 日志配置
-         *
-         * level: 日志级别，可选为：
-         *                 debug/info/notice/warning/error/critical/alert/emergency
-         * file：日志文件位置(绝对路径!!!)，要求可写权限
-         */
-        'log' => [
-            'level' => env('WECHAT_LOG_LEVEL', 'debug'),
-            'file' => env('WECHAT_LOG_FILE', storage_path('logs/wechat.log')),
-        ],
-    ],
+		/*
+			         * 日志配置
+			         *
+			         * level: 日志级别，可选为：
+			         *                 debug/info/notice/warning/error/critical/alert/emergency
+			         * file：日志文件位置(绝对路径!!!)，要求可写权限
+		*/
+		'log' => [
+			'level' => env('WECHAT_LOG_LEVEL', 'debug'),
+			'file' => env('WECHAT_LOG_FILE', storage_path('logs/wechat.log')),
+		],
+	],
 
-    /*
-     * 路由配置
-     */
-    'route' => [
-        /*
-         * 开放平台第三方平台路由配置
-         */
-        // 'open_platform' => [
-        //     'uri' => 'serve',
-        //     'action' => Overtrue\LaravelWeChat\Controllers\OpenPlatformController::class,
-        //     'attributes' => [
-        //         'prefix' => 'open-platform',
-        //         'middleware' => null,
-        //     ],
-        // ],
-    ],
+	/*
+		     * 路由配置
+	*/
+	'route' => [
+		/*
+			         * 开放平台第三方平台路由配置
+		*/
+		// 'open_platform' => [
+		//     'uri' => 'serve',
+		//     'action' => Overtrue\LaravelWeChat\Controllers\OpenPlatformController::class,
+		//     'attributes' => [
+		//         'prefix' => 'open-platform',
+		//         'middleware' => null,
+		//     ],
+		// ],
+	],
 
-    /*
-     * 公众号
-     */
-    'official_account' => [
-        'default' => [
-            'app_id' => env('WECHAT_OFFICIAL_ACCOUNT_APPID', 'wx05ffeeaa13c600da'),         // AppID
-            'secret' => env('WECHAT_OFFICIAL_ACCOUNT_SECRET', '5b2ae81d90a3b4cda229435296e7b4fb'),    // AppSecret
-            'token' => env('WECHAT_OFFICIAL_ACCOUNT_TOKEN', 'lzg2017'),           // Token
-            'aes_key' => env('WECHAT_OFFICIAL_ACCOUNT_AES_KEY', 'pfMaXUvbk46Rhii7wVpoJkUcxhSjQujZSsoe08sWYGw'),                 // EncodingAESKey
+	/*
+		     * 公众号
+	*/
+	'official_account' => [
+		'default' => [
+			'app_id' => env('WECHAT_OFFICIAL_ACCOUNT_APPID', ''), // AppID
+			'secret' => env('WECHAT_OFFICIAL_ACCOUNT_SECRET', ''), // AppSecret
+			'token' => env('WECHAT_OFFICIAL_ACCOUNT_TOKEN', ''), // Token
+			'aes_key' => env('WECHAT_OFFICIAL_ACCOUNT_AES_KEY', ''), // EncodingAESKey
 
-            /*
-             * OAuth 配置
-             *
-             * scopes：公众平台（snsapi_userinfo / snsapi_base），开放平台：snsapi_login
-             * callback：OAuth授权完成后的回调页地址(如果使用中间件，则随便填写。。。)
-             */
-            'oauth' => [
-                'scopes'   => array_map('trim', explode(',', env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_SCOPES', 'snsapi_userinfo'))),
-                'callback' => env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_CALLBACK', '/wxlogins'),
-            ],
-        ],
-    ],
+			/*
+				             * OAuth 配置
+				             *
+				             * scopes：公众平台（snsapi_userinfo / snsapi_base），开放平台：snsapi_login
+				             * callback：OAuth授权完成后的回调页地址(如果使用中间件，则随便填写。。。)
+			*/
+			'oauth' => [
+				'scopes' => array_map('trim', explode(',', env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_SCOPES', 'snsapi_userinfo'))),
+				'callback' => env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_CALLBACK', '/wxlogins'),
+			],
+		],
+	],
 
-    /*
-     * 开放平台第三方平台
-     */
-    // 'open_platform' => [
-    //     'default' => [
-    //         'app_id'  => env('WECHAT_OPEN_PLATFORM_APPID', ''),
-    //         'secret'  => env('WECHAT_OPEN_PLATFORM_SECRET', ''),
-    //         'token'   => env('WECHAT_OPEN_PLATFORM_TOKEN', ''),
-    //         'aes_key' => env('WECHAT_OPEN_PLATFORM_AES_KEY', ''),
-    //     ],
-    // ],
+	/*
+		     * 开放平台第三方平台
+	*/
+	// 'open_platform' => [
+	//     'default' => [
+	//         'app_id'  => env('WECHAT_OPEN_PLATFORM_APPID', ''),
+	//         'secret'  => env('WECHAT_OPEN_PLATFORM_SECRET', ''),
+	//         'token'   => env('WECHAT_OPEN_PLATFORM_TOKEN', ''),
+	//         'aes_key' => env('WECHAT_OPEN_PLATFORM_AES_KEY', ''),
+	//     ],
+	// ],
 
-    /*
-     * 小程序
-     */
-    'mini_program' => [
-        'default' => [
-            'app_id'  => env('WECHAT_MINI_PROGRAM_APPID', 'wx3fa706377424c2c9'),
-            'secret'  => env('WECHAT_MINI_PROGRAM_SECRET', '2ab070558b0be5d11cb339251cd5e28e'),
-            'token'   => env('WECHAT_MINI_PROGRAM_TOKEN', ''),
-            'aes_key' => env('WECHAT_MINI_PROGRAM_AES_KEY', ''),
-        ],
-    ],
+	/*
+		     * 小程序
+	*/
+	'mini_program' => [
+		'default' => [
+			'app_id' => env('WECHAT_MINI_PROGRAM_APPID', ''),
+			'secret' => env('WECHAT_MINI_PROGRAM_SECRET', ''),
+			'token' => env('WECHAT_MINI_PROGRAM_TOKEN', ''),
+			'aes_key' => env('WECHAT_MINI_PROGRAM_AES_KEY', ''),
+		],
+	],
 
-    /*
-     * 微信支付
-     */
-    'payment' => [
-        'default' => [
-            'sandbox'            => env('WECHAT_PAYMENT_SANDBOX', false),
-            'app_id'             => env('WECHAT_PAYMENT_APPID', 'wx05ffeeaa13c600da'),
-            'mch_id'             => env('WECHAT_PAYMENT_MCH_ID', '1491678032'),
-            'key'                => env('WECHAT_PAYMENT_KEY', 'g0moUivjfjKmoKlNvWeFhTDbdd5TSAYH'),
-            'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', storage_path('capem/apiclient_cert.pem')), // XXX: 绝对路径！！！！
-            'key_path'           => env('WECHAT_PAYMENT_KEY_PATH', storage_path('capem/apiclient_key.pem')),      // XXX: 绝对路径！！！！
-            'notify_url'         => '',                           // 默认支付结果通知地址
-        ],
-        // ...
-    ],
+	/*
+		     * 微信支付
+	*/
+	'payment' => [
+		'default' => [
+			'sandbox' => env('WECHAT_PAYMENT_SANDBOX', false),
+			'app_id' => env('WECHAT_PAYMENT_APPID', ''),
+			'mch_id' => env('WECHAT_PAYMENT_MCH_ID', ''),
+			'key' => env('WECHAT_PAYMENT_KEY', ''),
+			'cert_path' => env('WECHAT_PAYMENT_CERT_PATH', storage_path('capem/apiclient_cert.pem')), // XXX: 绝对路径！！！！
+			'key_path' => env('WECHAT_PAYMENT_KEY_PATH', storage_path('capem/apiclient_key.pem')), // XXX: 绝对路径！！！！
+			'notify_url' => '', // 默认支付结果通知地址
+		],
+		// ...
+	],
 
-    /*
-     * 企业微信
-     */
-    // 'work' => [
-    //     'default' => [
-    //         'corp_id' => 'xxxxxxxxxxxxxxxxx',
-    ///        'agent_id' => 100020,
-    //         'secret'   => env('WECHAT_WORK_AGENT_CONTACTS_SECRET', ''),
-    //          //...
-    //      ],
-    // ],
+	/*
+		     * 企业微信
+	*/
+	// 'work' => [
+	//     'default' => [
+	//         'corp_id' => 'xxxxxxxxxxxxxxxxx',
+	///        'agent_id' => 100020,
+	//         'secret'   => env('WECHAT_WORK_AGENT_CONTACTS_SECRET', ''),
+	//          //...
+	//      ],
+	// ],
 ];
